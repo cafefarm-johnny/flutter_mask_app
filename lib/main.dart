718c14468e86781f5e31f2279d8d70581e0549de
cafefarm-johnny <npcdja@gmail.com>
2023-01-24 22:31:30 +0900

    feat: 재고 수, 새로고침 기능 구현

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'model/store.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final stores = <Store>[];
+  bool isLoading = true;
+
+  Future<void> fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    const url =
+        'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode != 200) {
+      return;
+    }
+
+    final result = jsonDecode(response.body);
+    final jsonStores = result['stores'];
+
+    setState(() {
+      stores.clear();
+
+      jsonStores.forEach((e) {
+        stores.add(Store.fromJson(e));
+      });
+
+      isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('마스크 재고 있는 곳 : ${stores.length}곳'),
+        actions: [
+          // 새로고침
+          IconButton(
+            onPressed: fetch,
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: isLoading ? buildLoading() : buildBody(),
+    );
+  }
+
+  Widget buildLoading() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text('정보를 가져오는 중 입니다.'),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildBody() {
+    return ListView(
+      children: stores.map((e) {
+        return ListTile(
+          title: Text(e.name ?? ''),
+          subtitle: Text(e.addr ?? ''),
+          trailing: Text(e.remainStat ?? '매진'),
+        );
+      }).toList(),
+    );
+  }
+}
---
+++ lib/model/store.dart
@@ -0,0 +1,54 @@
+class Store {
+  String? code;
+  String? name;
+  String? addr;
+  String? type;
+  num? lat;
+  num? lng;
+  String? stockAt;
+  String? remainStat;
+  String? createdAt;
+
+  Store({
+    this.code,
+    this.name,
+    this.addr,
+    this.type,
+    this.lat,
+    this.lng,
+    this.stockAt,
+    this.remainStat,
+    this.createdAt,
+  });
+
+  Store.fromJson(Map<String, dynamic> json) {
+    code = json['code'];
+    name = json['name'];
+    addr = json['addr'];
+    type = json['type'];
+    lat = json['lat'];
+    lng = json['lng'];
+    stockAt = json['stock_at'];
+    remainStat = json['remain_stat'];
+    createdAt = json['created_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['code'] = this.code;
+    data['name'] = this.name;
+    data['addr'] = this.addr;
+    data['type'] = this.type;
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    data['stock_at'] = this.stockAt;
+    data['remain_stat'] = this.remainStat;
+    data['created_at'] = this.createdAt;
+    return data;
+  }
+
+  @override
+  String toString() {
+    return 'Store{code: $code, name: $name, addr: $addr, type: $type, lat: $lat, lng: $lng, stockAt: $stockAt, remainStat: $remainStat, createdAt: $createdAt}';
+  }
+}
